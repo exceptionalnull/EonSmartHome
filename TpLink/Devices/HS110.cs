a5ebaef539f2c59edc41cb62a32b42d4f8e25607
Mike D <mike@eondata.net>
2023-12-17 19:49:15 +0800

    adding more commands to the HS110 device class

--- TpLink/Devices/HS110.cs
+++ TpLink/Devices/HS110.cs
@@ -13,10 +13,22 @@ namespace EonData.SmartHome.TpLink.Devices
     {
         public HS110(SmartHomeClient protocolClient) : base(protocolClient) { }
         public HS110(string address) : this(new SmartHomeClient(address)) { }
+        /* system */
         public Task<SmartHomeDeviceInfoResponse> GetDeviceInfoAsync(CancellationToken cancellationToken) => client.GetDeviceInfoAsync(cancellationToken);
         public Task TurnPlugOffAsync(CancellationToken cancellationToken) => client.SetRelayStateAsync(false, cancellationToken);
-        public Task TurnPlugOnAsync(CancellationToken cancellationToken) => client.SetRelayStateAsync(true, cancellationToken);
-        public Task TurnLEDLightOn(CancellationToken cancellationToken) => client.SetLEDStateAsync(true, cancellationToken);
-        public Task TurnLEDLightOff(CancellationToken cancellationToken) => client.SetLEDStateAsync(false, cancellationToken);
+        public Task TurnPlugOnAsyncAsync(CancellationToken cancellationToken) => client.SetRelayStateAsync(true, cancellationToken);
+        public Task TurnLEDLightOnAsync(CancellationToken cancellationToken) => client.SetLEDStateAsync(true, cancellationToken);
+        public Task TurnLEDLightOffAsync(CancellationToken cancellationToken) => client.SetLEDStateAsync(false, cancellationToken);
+        public Task SetAliasAsync(string alias, CancellationToken cancellationToken) => client.SetAliasAsync(alias, cancellationToken);
+        public Task RebootAsync(int delay, CancellationToken cancellationToken) => client.RebootAsync(delay, cancellationToken);
+        /* wifi */
+
+        /* time */
+        public Task<SmartHomeTimeResponse> GetTimeAsync(CancellationToken cancellationToken) => client.GetTimeAsync(cancellationToken);
+        public Task<SmartHomeTimeZoneResponse> GetZimeZoneAsync(CancellationToken cancellationToken) => client.GetTimeZoneAsync(cancellationToken);
+        /* emeter */
+        public Task<IEnumerable<SmartHomeEMeterDayStat>> GetDailyStatsAsync(int year, int month, CancellationToken cancellationToken) => client.GetDayStatsAsync(year, month, cancellationToken);
+        public Task<IEnumerable<SmartHomeEMeterMonthStat>> GetMonthlyStatsAsync(int year, CancellationToken cancellationToken) => client.GetMonthStatsAsync(year, cancellationToken);
+        public Task<SmartHomeEMeterRealtimeResponse> GetRealtimeStatsAsync(CancellationToken cancellationToken) => client.GetRealtimeMetricsAsync(cancellationToken);
     }
 }
