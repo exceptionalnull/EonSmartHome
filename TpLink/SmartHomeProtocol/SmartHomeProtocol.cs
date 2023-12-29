8168583bc9af91487f2835289671d2ca574543ca
Mike D <mike@eondata.net>
2023-12-29 11:27:33 +0800

    cleaned up and refined device discovery

--- TpLink/SmartHomeProtocol/SmartHomeProtocol.cs
+++ TpLink/SmartHomeProtocol/SmartHomeProtocol.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -23,17 +24,12 @@ namespace EonData.SmartHome.TpLink.SmartHomeProtocol
         /// <summary>
         /// Time to wait when listening for responses.
         /// </summary>
-        public TimeSpan DiscoveryTimeout { get; set; } = TimeSpan.FromSeconds(5);
+        public TimeSpan DiscoveryTimeout { get; set; } = TimeSpan.FromSeconds(10);
 
         /// <summary>
         /// Delay between sending discovery packets.
         /// </summary>
         public TimeSpan DiscoveryDelay { get; set; } = TimeSpan.FromMilliseconds(125);
-
-        /// <summary>
-        /// Number of timeouts before giving up on discovery.
-        /// </summary>
-        public int DiscoveryRetries { get; set; } = 2;
 
         /// <summary>
         /// Gets or sets the number of discovery packets to send when discovering devices.
@@ -47,20 +43,14 @@ namespace EonData.SmartHome.TpLink.SmartHomeProtocol
         /// <returns>JSON data response string</returns>
         public async Task<string> SendDataAsync(string address, string data, CancellationToken cancellationToken)
         {
-            string response;
-
-            using (var tcp = new TcpClient())
-            {
-                await tcp.ConnectAsync(address, Port, cancellationToken);
-                await using var netStream = tcp.GetStream();
-                var dataBytes = SmartHomeCypher.Encrypt(data, true);
-                await netStream.WriteAsync(dataBytes, 0, dataBytes.Length, cancellationToken);
-                var buffer = new byte[ReadBufferSize];
-                await netStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
-                response = SmartHomeCypher.Decrypt(buffer, true);
-            }
-
-            return response;
+            using var tcp = new TcpClient();
+            await tcp.ConnectAsync(address, Port, cancellationToken);
+            await using var netStream = tcp.GetStream();
+            var dataBytes = SmartHomeCypher.Encrypt(data, true);
+            await netStream.WriteAsync(dataBytes, 0, dataBytes.Length, cancellationToken);
+            var buffer = new byte[ReadBufferSize];
+            await netStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
+            return SmartHomeCypher.Decrypt(buffer, true);
         }
 
         /// <summary>
@@ -68,14 +58,14 @@ namespace EonData.SmartHome.TpLink.SmartHomeProtocol
         /// </summary>
         /// <param name="cancellationToken"></param>
         /// <returns>Device addresses.</returns>
-        public async Task<IEnumerable<string>> DiscoverDevicesAsync(CancellationToken cancellationToken)
+        public async Task<IReadOnlyDictionary<string, SmartHomeDeviceInfoResponse>> DiscoverDevicesAsync(CancellationToken cancellationToken)
         {
-            using UdpClient udp = new UdpClient(Port) { EnableBroadcast = true };
-            var sendTask = SendDiscoveryPacketsAsync(udp, cancellationToken);
-            var listenTask = ListenForDevicesAsync(udp, cancellationToken);
-            await Task.WhenAll(sendTask, listenTask);
-            var devices = await listenTask;
-            return devices.Select(x => x.Address.ToString());
+            using UdpClient udp = new(Port) { EnableBroadcast = true };
+            using SemaphoreSlim packetSemaphore = new(0, 1);
+            var sendingPackets = SendDiscoveryPacketsAsync(udp, packetSemaphore, cancellationToken);
+            var listeningForDevices = ListenForDevicesAsync(udp, packetSemaphore, cancellationToken);
+            await Task.WhenAll(sendingPackets, listeningForDevices);
+            return await listeningForDevices;
         }
 
         /// <summary>
@@ -83,17 +73,16 @@ namespace EonData.SmartHome.TpLink.SmartHomeProtocol
         /// </summary>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        private async Task SendDiscoveryPacketsAsync(UdpClient udp, CancellationToken cancellationToken)
+        private async Task SendDiscoveryPacketsAsync(UdpClient udp, SemaphoreSlim discoverySemaphore, CancellationToken cancellationToken)
         {
             // create discovery packet
-            IPEndPoint broadcastEndpoint = new IPEndPoint(IPAddress.Broadcast, Port);
-            var command = new SmartHomeCommand<SmartHomeDeviceInfoResponse>("system", "get_sysinfo");
-            var commandJson = command.GetCommandJson();
-            var discoveryPacket = SmartHomeCypher.Encrypt(commandJson, false);
-
+            IPEndPoint broadcastEndpoint = new(IPAddress.Broadcast, Port);
+            SmartHomeCommand<SmartHomeDeviceInfoResponse> command = new("system", "get_sysinfo");
+            var discoveryPacket = SmartHomeCypher.Encrypt(command.GetCommandJson(), false);
             for (int i = 0; i < DiscoveryPackets; i++)
             {
                 await udp.SendAsync(discoveryPacket, discoveryPacket.Length, broadcastEndpoint);
+                discoverySemaphore.Release();
                 await Task.Delay(DiscoveryDelay, cancellationToken); // stagger the sending of packets
             }
         }
@@ -103,29 +92,41 @@ namespace EonData.SmartHome.TpLink.SmartHomeProtocol
         /// </summary>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        private async Task<IEnumerable<IPEndPoint>> ListenForDevicesAsync(UdpClient udp, CancellationToken cancellationToken)
+        private async Task<IReadOnlyDictionary<string, SmartHomeDeviceInfoResponse>> ListenForDevicesAsync(UdpClient udp, SemaphoreSlim discoverySemaphore, CancellationToken cancellationToken)
         {
-            var command = new SmartHomeCommand<SmartHomeDeviceInfoResponse>("system", "get_sysinfo");
+            // wait for discovery packets to start being sent
+            await discoverySemaphore.WaitAsync(cancellationToken);
+
+            SmartHomeCommand<SmartHomeDeviceInfoResponse> command = new("system", "get_sysinfo");
             List<IPEndPoint> deviceAddresses = new();
-            int timeouts = 0;
-            while (timeouts < DiscoveryRetries)
+            Dictionary<string, SmartHomeDeviceInfoResponse> result = new();
+            // listen for device responses
+            while (true)
             {
                 if (udp.Client.Poll(DiscoveryTimeout, SelectMode.SelectRead))
                 {
+                    // receive and process response
                     UdpReceiveResult response = await udp.ReceiveAsync(cancellationToken);
-                    string responseString = SmartHomeCypher.Decrypt(response.Buffer, false);
-                    var responseValue = command.GetResponseValue(responseString);
-                    if (responseValue.DeviceId != null && !deviceAddresses.Contains(response.RemoteEndPoint))
+                    var deviceInfo = command.GetResponseValue(SmartHomeCypher.Decrypt(response.Buffer, false));
+
+                    // this receiver will also catch discovery packets but they will have an empty device info object
+                    if (deviceInfo.HardwareId != null)
                     {
-                        deviceAddresses.Add(response.RemoteEndPoint);
+                        // sending multiple packets can cause multiple replies. only add the first one.
+                        string deviceAddress = response.RemoteEndPoint.Address.ToString();
+                        if (!result.ContainsKey(deviceAddress))
+                        {
+                            result.Add(deviceAddress, deviceInfo);
+                        }
                     }
                 }
                 else
                 {
-                    timeouts++;
+                    // no response
+                    break;
                 }
             }
-            return deviceAddresses;
+            return result.AsReadOnly();
         }
     }
 }
